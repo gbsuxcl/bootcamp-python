e884dfe691297806081671ae8f0059b65a645969
Gabrielle <gabrielle.urcioli@gmail.com>
2026-05-18 01:00:46 -0300

    feat: adicionar a resolução do exercicio 4 da aula controle de fluxo

--- controle_de_fluxo/exercicio_controle_de_fluxo.py
+++ controle_de_fluxo/exercicio_controle_de_fluxo.py
@@ -46,3 +46,27 @@ log = {
 
 if log["level"] == "ERROR":
     print(log["message"])
+
+
+### Exercício 4: Validação de Dados de Entrada
+# Antes de processar os dados de usuários em um sistema de recomendação,
+# você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha
+# fornecido um email válido. Escreva um programa que valide essas condições
+# e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+idade = 20
+email = "user@example.com"
+
+while idade != True or email != True:
+    try:
+
+        if not 18 <= idade <= 65:
+            idade == False
+            print("Idade invalida. Insira a idade novamente")
+        elif "@" not in email or "." not in email:
+            email == False
+            print("Email invalido. Insira o email novamente")
+        else:
+            idade == True and email == True
+            print("Dados de usuario validos")
+    except TypeError as e:
+        print(f"{e}")
