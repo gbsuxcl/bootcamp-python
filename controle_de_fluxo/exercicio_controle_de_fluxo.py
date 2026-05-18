d8c6efde0bec0ae523c005eda033eb8a061faae8
Gabrielle <gabrielle.urcioli@gmail.com>
2026-05-18 00:31:31 -0300

    feat: adicionar a resolução do exercicio 2 da aula controle de fluxo

--- controle_de_fluxo/exercicio_controle_de_fluxo.py
+++ controle_de_fluxo/exercicio_controle_de_fluxo.py
@@ -10,3 +10,23 @@ if preco > 0 and quantidade > 0:
     print("Dados válidos")
 else:
     print("Dados inválidos")
+
+
+### Exercício 2: Classificação de Dados de Sensor
+# Imagine que você está trabalhando com dados de sensores IoT.
+# Os dados incluem medições de temperatura. Você precisa classificar cada leitura
+# como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# Temperatura < 18°C é 'Baixa'
+# 18°C <= Temperatura <= 25°C é 'Normal'
+# Temperatura > 25°C é 'Alta'
+
+temperatura = 30
+
+if temperatura < 18:
+    print("Temperatura Baixa")
+elif temperatura >= 18 and temperatura <= 25:
+    print("Temperatura Normal")
+elif temperatura > 25:
+    print("Temperatura Alta")
+else:
+    print("Temperatura não identificada")
