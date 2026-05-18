cfc6f93fcba665b98bcb652ba7fa6d2460ba9b54
Gabrielle <gabrielle.urcioli@gmail.com>
2026-05-18 01:46:12 -0300

    feat: adicionar a resolução do exercicio 9 da aula controle de fluxo

--- controle_de_fluxo/exercicio_controle_de_fluxo.py
+++ controle_de_fluxo/exercicio_controle_de_fluxo.py
@@ -134,3 +134,35 @@ for usuario in usuarios:
         usuarios_validos.append(usuario)
 
 print(f"Usuários válidos: {usuarios_validos}")
+
+
+### Exercício 9. Extração de Subconjuntos de Dados
+# Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
+numeros = range(1, 11)
+pares = []
+
+for num in numeros:
+    if num % 2 == 0:
+        pares.append(num)
+print(f"Pares encontrados {pares}")
+
+
+### Exercício 10. Agregação de Dados por Categoria
+# Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+
+### Exercícios com WHILE
+
+### Exercício 11. Leitura de Dados até Flag
+# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+
+### Exercício 12. Validação de Entrada
+# Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+
+### Exercício 13. Consumo de API Simulado
+# Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+
+### Exercício 14. Tentativas de Conexão
+# Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+
+### Exercício 15. Processamento de Dados com Condição de Parada
+# Processar itens de uma lista até encontrar um valor específico que indica a parada.
