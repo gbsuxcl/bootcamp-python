e545a726144343f26e53067a3bbcb7c7d618be5c
Gabrielle <gabrielle.urcioli@gmail.com>
2026-05-17 22:30:14 -0300

    feat: adiciona a resolução do exercicio 22

--- aula_02/exercicio_try_except_and_if.py
+++ aula_02/exercicio_try_except_and_if.py
@@ -1,7 +1,6 @@
 # #### try-except e if
 
 # 21: Conversor de Temperatura
-
 try:
     celsius = float(input("Digite a temperatura em graus Celsius: "))
 
@@ -14,6 +13,26 @@ except ValueError as e:
 
 
 # 22: Verificador de Palíndromo
+import re
+
+frase = input("Digite uma frase: ")
+
+# 01. Limpeza
+limpeza = re.sub(r"[^A-Za-z0-9]", "", frase)
+
+# 02. Remove espaços
+remove_espacos = limpeza.replace(" ", "")
+
+# 03. Inverte a string
+inversao = remove_espacos[::-1]
+
+# 04. Compara as frases
+if frase == frase[::-1]:
+    print("A frase é um políndromo")
+else:
+    print("A frase não é um políndromo")
+
+
 # 23: Calculadora Simples
 # 24: Classificador de Números
 # 25: Conversão de Tipo com Validação
