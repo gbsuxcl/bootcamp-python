a9878103df0b1e763fed83996afb6aab3ba57301
Gabrielle <gabrielle.urcioli@gmail.com>
2026-05-18 13:40:55 -0300

    feat: resolução dos exercicios do tipo dicionario

--- 04_tipos_complexos_e_type_hint/exercicios_tipos.py
+++ 04_tipos_complexos_e_type_hint/exercicios_tipos.py
@@ -40,6 +40,7 @@ numeros: list[int] = [10, 20, 30, 40, 50]
 media = sum(numeros) / len(numeros)
 print(media)
 
+
 # 10. Divisão de Dados em Grupos
 # Objetivo: Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
 valores: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -54,15 +55,58 @@ print(f"Ímpares: {valores_impares}")
 # ### Exercícios com Dicionários
 # 11. Atualização de Dados
 # Objetivo: Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+produtos = [
+    {"id": 1, "nome": "Teclado", "preço": 100},
+    {"id": 2, "nome": "Mouse", "preço": 80},
+    {"id": 3, "nome": "Monitor", "preço": 300},
+]
+
+for produto in produtos:
+    if produto["id"] == 2:
+        produto["preço"] = 50
+
+print(produtos)
+
 
 # 12. Fusão de Dicionários
 # Objetivo: Dados dois dicionários, fundi-los em um único dicionário.
+dicionario1 = {"a": 1, "b": 2}
+dicionario2 = {"c": 3, "d": 4}
+
+dicionario_unico = {**dicionario1, **dicionario2}
+
 
 # 13. Filtragem de Dados em Dicionário
 # Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
+
+estq_filtrado = {}
+
+for produto, quantidade in estoque.items():
+    if quantidade > 0:
+        estq_filtrado[produto] = quantidade
+print(estq_filtrado)
+
 
 # 14. Extração de Chaves e Valores
 # Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
+dicionario: dict = {"a": 1, "b": 2, "c": 3}
+
+chaves: list = dicionario.keys()
+valores: list = dicionario.values()
+
+print(f"Chaves: {chaves}")
+print(f"Valores: {valores}")
 
 # 15. Contagem de Frequência de Itens
 # Objetivo: Dada uma string, contar a frequência de cada caractere usando um dicionário.
+texto = "Data creates possibilities"
+frequencia = {}
+
+for x in texto.lower():
+    if x in frequencia:
+        frequencia[x] += 1
+    else:
+        frequencia[x] = 1
+
+print(frequencia)
