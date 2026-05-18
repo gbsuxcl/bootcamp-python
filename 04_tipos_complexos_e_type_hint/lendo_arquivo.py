dfc8a2de16033b393df0df0489eae8dc660d4e9b
Gabrielle <gabrielle.urcioli@gmail.com>
2026-05-18 16:57:31 -0300

    feat: resolução dos exercicios do tipo aula 4

---
+++ 04_tipos_complexos_e_type_hint/lendo_arquivo.py
@@ -0,0 +1,14 @@
+import csv  # biblioteca nativa do python
+
+caminho_arquivo = "arquivo.csv"
+
+arquivo_csv: list = []
+
+with open(file=caminho_arquivo, mode="r", encoding="utf-8") as arquivo:
+
+    leitor_csv = csv.DictReader(arquivo)
+
+    for linha in leitor_csv:
+        arquivo_csv.append(linha)
+
+print(arquivo_csv)
